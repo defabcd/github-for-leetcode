f71dbdbc7b35d88e524e3ec40e741b0c1bd50e5f
defabcd <164007677+defabcd@users.noreply.github.com>
2024-06-22 11:33:57 +0900

    Add files via upload

---
+++ 2427. Number of Common Factors.py
@@ -0,0 +1,7 @@
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        c = 0
+        for i in range(1, min([a, b]) + 1) :
+            if not a % i and not b % i :
+                c += 1
+        return c
